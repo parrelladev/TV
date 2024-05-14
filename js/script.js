13753dc889743dd61166340c322a0c8a98d7d708
Ângelo <angelo.parrella@live.com>
2024-05-13 21:55:04 -0300

    Estrutura

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function changeVideo(embedCode) {
+    document.getElementById('videoPlayer').src = embedCode;
+}
+
+document.getElementById('canal1').addEventListener('click', function() {
+    changeVideo("CÓDIGO_DE_INCORPORAÇÃO_BAND_SPORTS");
+});
+
+document.getElementById('canal2').addEventListener('click', function() {
+    changeVideo("CÓDIGO_DE_INCORPORAÇÃO_ESPN");
+});
+
+document.getElementById('canal3').addEventListener('click', function() {
+    changeVideo("CÓDIGO_DE_INCORPORAÇÃO_PREMIERE");
+});
+
+document.getElementById('canal4').addEventListener('click', function() {
+    changeVideo("CÓDIGO_DE_INCORPORAÇÃO_SPOR_TV");
+});
+
+// Adicione mais event listeners conforme necessário para outros canais
+
+
+function changeVideo(url) {
+    // Decodifica a URL
+    const decodedUrl = decodeURIComponent(url);
+    document.getElementById('videoPlayer').src = decodedUrl;
+}
+
+document.getElementById('canal1').addEventListener('click', function() {
+    // Substitua "urlCodificada" pela URL codificada que você forneceu
+    changeVideo("//%E3%83%87-%E3%83%B3-%E3%83%83-%E3%82%AF-%E3%82%B9-%E3%83%B3-%E3%83%87-%E3%83%89-%E3%83%89-%E3%83%AB-%E3%83%9C-%E3%83%A9-%E3%83%AB%E3%82%B0%E3%83%AC%E3%83%95%E3%83%88-%E3%83%96%E3%83%A0%E3%83%95%E3%82%AF%E3%83%88%E3%83%97%E3%83%89%E3%83%A9%E3%82%A2.%E3%82%B8-1l1-%E3%82%B0.%E3%83%83-22-%E3%82%AF-11-%E3%82%B9-33-%E3%83%AB-99-%E3%83%97-75-%E3%82%BE--%E3%82%A8--%E3%83%96--%E3%82%B9-%E3%83%83.%E3%82%AF.%E3%82%B9.%E3%82%BA.%E3%82%AF.%E3%82%B8.%E3%82%B7%E3%82%B0%E3%83%8A%E3%83%AB%E3%83%91%E3%83%96%E3%83%AA%E3%82%B3-%E5%85%AC%E5%85%B1%E3%81%AE%E6%A8%99%E8%AD%98-%E3%83%90%E3%83%AC%E3%82%A6%E3%83%89%E3%83%83%E3%83%88%E3%82%AF%E3%82%A6%E3%82%B0%E3%83%88%E3%83%83%E3%83%88%E3%82%BA%E3%83%92.%E3%82%BB%E3%83%BC%E3%83%AB/player3/ch.php?canal=bandsports");
+});
